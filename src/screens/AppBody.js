8e9e2456aed4d7810c395177d729820a5d818eb1
Mithilesh Tarkar <mithilesh.tarkar@ves.ac.in>
2018-03-16 13:52:15 +0530

    trying to call function from button in reactJS

--- src/screens/AppBody.js
+++ src/screens/AppBody.js
@@ -3,24 +3,37 @@ import React, { Component } from 'react';
 
 
 class AppBody extends Component {
+
+
+        getdetails(){
+
+                alert('helloworld');
+                console.log('clicked');
+
+        }
+
+
   render() {
     return (
                 
-        <div class="jumbotron">
-                <h1 class="display-3"> Learn Guitar </h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr class="my-4" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <p class="lead">
-                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-                </p>
-                <h1 class="display-3">Learn Piano</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr class="my-4" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <p class="lead">
-                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-                </p>
+        <div class="card">
+               
+            <div class="card">
+
+
+                <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap" />
+
+                
+                <div class="card-body">
+                    
+                    <h4 class="card-title"> Get Details of City</h4>
+                    
+                    <p class="card-text"> </p>
+                    <button class="btn btn-primary" onPress={()=>this.getdetails()} >Click to Get Details</button>
+                </div>
+
+            </div>
+
          </div>
          
          
